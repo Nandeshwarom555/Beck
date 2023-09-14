5ef289467426ba5a514bc409b3cc4cc1dffeaad0
@om56577 <116886444+Nandeshwarom555@users.noreply.github.com>
2023-09-14 11:26:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://te.legra.ph/file/f83017d890b1b692a673a.jpg https://te.legra.ph/file/72602fb1f87727dde571b.jpg https://te.legra.ph/file/e37fbea2190470aaf4bc1.jpg https://te.legra.ph/file/9f4f70db519f1bdbcb27b.jpg https://te.legra.ph/file/72d74f632a2317f21136b.jpg https://te.legra.ph/file/018e5893fdaabe0f1cef4.jp')).split()
 NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/d3e056bd633402ee6c1a6.jpg")
-SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/314928baed9a9c277072f.jpg")
+SPELL_IMG = environ.get('SPELL_IMG',"https://graph.org/file/fb2eebed755733f2a7b59.jpg")
 
 # Welcome area
 MELCOW_IMG = environ.get('MELCOW_IMG',"https://telegra.ph/file/e54cae941b9b81f13eb71.jpg")
